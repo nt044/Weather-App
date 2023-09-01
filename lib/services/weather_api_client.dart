3cd806ab43eb8424bec8aac6df4c78b4172af5c6
Neha✨ <neha.thakran2001@gmail.com>
2023-09-01 17:40:45 +0530

    Add files via upload

---
+++ lib/services/weather_api_client.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/model/weather_model.dart';
+
+class WeatherApiClient {
+  Future<Weather>? getCurrentWeather(String? location) async {
+    var endpoint = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=54fb688a848f4a5331a15c82b72023ca&units=metric");
+
+    var response = await http.get(endpoint);
+    var body = jsonDecode(response.body);
+    print(Weather.fromJson(body).cityName);
+    return Weather.fromJson(body);
+  }
+}
